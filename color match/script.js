75e0a9e7724b8e1f62aea7e6e476ce59bf68496e
Kovács Mihály <117653038+Misike28@users.noreply.github.com>
2024-11-19 14:11:11 +0100

    Add files via upload

---
+++ color match/script.js
@@ -0,0 +1,9 @@
+const nevek = ['aka','orenji', 'kiiro', 'midori', 'ao','pinku','hai','kuro'];
+const szinek = ['crimson','orange','gold','forestgreen','dodgerblue','pink','gray','black'];
+let szin = "";
+
+
+function generate(){
+    let rnd = Math.floor(Math.random()*szinek.length)
+    document.getElementById
+}
